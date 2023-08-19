1c93a0455716f4c521857339c8578a7175ca6009
Amirhassan <amirhassan.moosavi@gandom.co>
2023-08-19 23:29:44 +0330

    init commit

---
+++ app/src/main/java/com/amir/batmanmovies/ui/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.amir.batmanmovies.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amir.batmanmovies.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
